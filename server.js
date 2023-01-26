30f6612a75f5692a0b68ab8d9085c63eca8285aa
Mahmoud <barghoutmahmoud2@gmail.com>
2023-01-26 16:14:17 +0200

    created a basic server installed and added express,cors and body-parser. with get and post methods. attempted to use it from client side by creating a client side fetch function. it is still not working

--- server.js
+++ server.js
@@ -2,18 +2,40 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+const port = 8080;
+const server = app.listen(port, listening);
+function listening() {
+  console.log("server running");
+  console.log(`running on localhost: ${port}`);
+}
+
+app.post("/all", function (req, res) {
+  console.log(req.body);
+  return "hello";
+});
+
+const Data = [];
+app.post("/addMovie", addMovie);
+
+function addMovie(req, res) {
+  Data.push(req.body);
+  console.log(Data);
+}
